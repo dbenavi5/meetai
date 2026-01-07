6215db4a4f96c1afeb9a17197c74aa36ba5e206f
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2026-01-07 12:50:18 -0800

    11: Agents Form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,37 +1,54 @@
+import { Suspense } from "react";
+import { headers } from "next/headers";
+import { ErrorBoundary } from "react-error-boundary";
+import { redirect } from "next/navigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { ListHeader } from "@/modules/agents/ui/components/list-header";
 import { AgentsView } from "@/modules/agents/ui/views/agents-view";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
 const AgentsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense
-        fallback={
-          <LoadingState
-            title="Loading Agents"
-            description="This may take a few seconds..."
-          />
-        }
-      >
-        <ErrorBoundary
+    <>
+      <ListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense
           fallback={
-            <ErrorState
-              title="Failed to load agents"
-              description="There was an error loading the agents. Please try again later."
+            <LoadingState
+              title="Loading Agents"
+              description="This may take a few seconds..."
             />
           }
         >
-          <AgentsView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+          <ErrorBoundary
+            fallback={
+              <ErrorState
+                title="Failed to load agents"
+                description="There was an error loading the agents. Please try again later."
+              />
+            }
+          >
+            <AgentsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 
