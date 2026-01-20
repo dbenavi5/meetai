53ab3e576baac12717bc7079eb8090b713677d22
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2026-01-20 13:39:55 -0800

    23: Connecting Agents

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -142,7 +142,6 @@ export const meetingsRouter = createTRPCRouter({
                 })
                 .returning();
 
-            // TODO: Create Stream Call, Upsert Stream users
             const call = streamVideo.video.call('default', createdMeeting.id);
 
             await call.create({
